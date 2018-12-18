c0cf92626b9436d70163a662554ed55bd219a1ac
LaoBing_Fung <FLB_B401@163.com>
2018-12-19 00:12:32 +0800

    optimize structure

--- src/best-charge.js
+++ src/best-charge.js
@@ -1,5 +1,4 @@
 'use strict';
-
 var loadAllItems = require("items.js");
 var loadPromotions = require("promotions");
 
@@ -7,24 +6,25 @@ function bestCharge(selectedItems) {
   return /*TODO*/;
 }
 
-function getItemsList(originalArr){
-    // return Arr like [["ITEM0001", 1], ["ITEM0013", 2], ["ITEM0022", 1]]
+function getItemsList(originalArr) {
+  // return Arr like
+  // [{id: "ITEM0001", count: 1}, {id: "ITEM0013", count: 2}, {id: "ITEM0022", count: 1}]
 }
 
-function computeItemsPriceList(itemslist){
-    //return Arr like [["ITEM0001", 1, 18.00], ["ITEM0013", 2, 12.00], ["ITEM0022", 1, 8.00]]
+function computeItemsPriceList(itemslist) {
+  //return Arr like
+  //[{id: "ITEM0001", count: 1, subtotal: 18.00}, {id: "ITEM0013", count: 2, subtotal: 12.00}, {id: "ITEM0022", count: 1, subtotal: 8.00}]
 }
 
-function computePromotion(itemsPriceList){
-    //return Object like {type: '满30减6元', discount: 6}
-    //or {type: '指定菜品半价',items: ['ITEM0001', 'ITEM0022'], discount: 13}
+function computePromotion(itemsPriceList) {
+  //return Object like {type: '满30减6元', discount: 6.00}
+  //or {type: '指定菜品半价',items: ['ITEM0001', 'ITEM0022'], discount: 13.00}
 }
 
-function computeTotalPrice(itemsPriceList, promotion){
-    //return Number totalPrice
+function computeTotalPrice(itemsPriceList, promotion) {
+  //return Number totalPrice
 }
 
-function getBill(itemsPriceList, promotion, totalPrice){
-    //return final bill to print
+function getBill(itemsPriceList, promotion, totalPrice) {
+  //return final bill to print
 }
-
