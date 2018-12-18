c0d4679ebeb8ceb1f5f31ecbf98dfdc3001a9e3a
LaoBing_Fung <FLB_B401@163.com>
2018-12-19 00:22:50 +0800

    convert input string to array of objects

--- src/best-charge.js
+++ src/best-charge.js
@@ -7,8 +7,14 @@ function bestCharge(selectedItems) {
 }
 
 function getItemsList(originalArr) {
-  // return Arr like
-  // [{id: "ITEM0001", count: 1}, {id: "ITEM0013", count: 2}, {id: "ITEM0022", count: 1}]
+  let itemsList = originalArr.map(function(curStr) {
+    let order = curStr.split(" x ");
+    return {
+      id: order[0],
+      count: order[1]
+    };
+  });
+  return itemsList;
 }
 
 function computeItemsPriceList(itemslist) {
