06cfb5399b7ce674c037cbecccc7f86e053ada2b
LaoBing_Fung <FLB_B401@163.com>
2018-12-19 20:50:48 +0800

    output the final bill

--- src/best-charge.js
+++ src/best-charge.js
@@ -45,7 +45,7 @@ function computePromotions(itemsPriceList) {
     "discount": discount6Per30
   } : {
     "type": '指定菜品半价',
-    "items": discountHalf.specialItems,
+    "names": discountHalf.specialItems,
     "discount": discountHalf.discount
   }
   return promotionsObj;
@@ -96,7 +96,16 @@ function getBill(itemsPriceList, promotion, totalPrice) {
   if (promotion.type === '满30减6元') {
     output += '使用优惠:\n满30减6元，省' + promotion.discount.toString() + '元\n';
   } else if (promotion.type === '指定菜品半价') {
-    output += '使用优惠:\n指定菜品半价' +
+    output += '使用优惠:\n指定菜品半价(';
+    promotion.names.forEach(function(curStr, curI, arr) {
+        output += curStr;
+        output += (curI === (arr.length - 1)) ? '，' :')，省';
+    })
+    output += promotion.discount.toString() + '元\n';
   }
+  output += '-----------------------------------\n';
+  output += '总计：' + totalPrice.toString() + '元\n';
+  output += '===================================';
   //return final bill to print
+  return output;
 }
